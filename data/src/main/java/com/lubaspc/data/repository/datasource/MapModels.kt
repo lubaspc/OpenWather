565dd1d0a00f16d0fe96e1dd3b87bdcd1b67adf3
lubinpc <lubinpc@gmail.com>
2021-03-14 15:13:30 -0600

    correct bug click

--- data/src/main/java/com/lubaspc/data/repository/datasource/MapModels.kt
+++ data/src/main/java/com/lubaspc/data/repository/datasource/MapModels.kt
@@ -4,11 +4,14 @@ import com.google.gson.Gson
 import com.google.gson.JsonParser
 import com.lubaspc.data.repository.room.entities.Weather
 import com.lubaspc.domain.model.Test
+import java.util.*
 
 fun Weather.toTest(): Test =
-    Gson().fromJson<Test>(this.modelString,Test::class.java).apply {
+    Gson().fromJson<Test>(this.modelString, Test::class.java).apply {
         createdAt = this@toTest.createdAt
     }
 
-fun com.lubaspc.data.repository.retrofit.models.Test.toTestDomain(): Test
-    = Gson().fromJson<Test>(this.toString(),Test::class.java)
+fun com.lubaspc.data.repository.retrofit.models.Test.toTestDomain(): Test =
+    Gson().fromJson<Test>(this.toString(), Test::class.java).apply {
+        createdAt = Date()
+    }
